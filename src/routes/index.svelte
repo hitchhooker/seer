92259d001e5ce99582279518db34da071d55221b
Tommi <tommi@niemi.lol>
2021-11-17 15:11:49 +0000

    replace css with inline and add types

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -35,8 +35,9 @@
 
 	// landing layer image
 	const layers = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-	let y;
+	let y:number;
 </script>
+
 
 <!--TODO: create parallax component to libraries -->
 <svelte:window bind:scrollY={y} />
@@ -44,6 +45,7 @@
 <div class="parallax-container">
 	{#each [0, 1, 2, 3, 4, 5, 6, 7, 8] as layer}
 		<img
+			class="absolute top-0 left-0 transform w-full"
 			style="transform: translate(0,{(-y * layer) / (layers.length - 1)}px)"
 			src="parallax/parallax{layer}.png"
 			alt="parallax layer number {layer}"
@@ -51,13 +53,11 @@
 	{/each}
 </div>
 
-<div class="text">
-	<span style="opacity: {1 - Math.max(0, y / 40)}"> scroll down </span>
+<div class="relative h-250vh">
 
-	<div class="foreground">
-		You have scrolled {y} pixels
+	<div class="absolute top-650px left-0 w-full h-[calc(100%-650px) foreground">
 
-		<h1>Welcome to Zeitgeist Seer program.</h1>
+		<h1>The Zeitgeist Seer Program</h1>
 		<div class="mb-1">
 			<p>
 				Site is currently under construction so please. Visit <a href="https://zeitgeist-seer.com/"
@@ -65,38 +65,40 @@
 				> to read more and participate.
 			</p>
 		</div>
-
 		<h1>News</h1>
 
-		<ul>
+		<ul class="">
 			{#each posts as { slugPage, metadata: { title, slug } }}
 				<li>
 					<a
 						href={`${base}/${linkSlug(slug, slugPage)}`}
 						sveltekit:prefetch
-						class="link-no-underline">{title}</a
+						class="link-no-underline text-yellow-400 hover:text-yellow-100">{title}</a
 					>
 				</li>
 			{/each}
 		</ul>
+
 	</div>
 </div>
+
 
 <style>
 	.parallax-container {
 		position: fixed;
+		top:0;
 		width: 2400px;
-		height: 712px;
+		height: 762px;
 		left: 50%;
 		transform: translate(-50%, 0);
 	}
-	.parallax-container img {
+	/*.parallax-container img {
 		position: absolute;
 		top: 0;
 		left: 0;
 		width: 100%;
 		will-change: transform;
-	}
+	}*/
 	.parallax-container img:last-child::after {
 		content: '';
 		position: absolute;
@@ -104,38 +106,16 @@
 		height: 100%;
 		background: rgb(45, 10, 13);
 	}
-	.text {
-		position: relative;
-		width: 100%;
-		height: 300vh;
-		color: rgb(220, 113, 43);
-		text-align: center;
-		padding: 4em 0.5em 0.5em 0.5em;
-		box-sizing: border-box;
-		pointer-events: none;
-	}
-	span {
-		display: block;
-		font-size: 1em;
-		text-transform: uppercase;
-		will-change: transform, opacity;
-	}
 	.foreground {
-		position: absolute;
-		top: 711px;
-		left: 0;
 		width: 100%;
-		height: calc(100% - 712px);
+		height: calc(100% - 762px);
 		background-color: rgb(32, 0, 1);
 		color: rgb(220, 113, 43);
 		padding: 5vh 0 0 0;
 	}
-	.foreground a {
-		color: rgb(253, 174, 51)
-	}
 	:global(body) {
 		margin: 0;
 		padding: 0;
-		background-color: rgb(253, 174, 51);
+		background-color: rgb(32, 0, 1);
 	}
 </style>
