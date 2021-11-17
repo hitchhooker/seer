b7b4025bfa15e90fec6367ab3c018e3d454656a7
Tommi <tommi@niemi.lol>
2021-11-17 01:27:06 +0000

    gh-pages info updated

--- gh-pages.js
+++ gh-pages.js
@@ -6,11 +6,11 @@ publish(
     'build', // path to public directory
     {
         branch: 'gh-pages',
-        repo: 'https://github.com/el3um4s/memento-sveltekit-and-github-pages.git', // Update to point to your repository
+        repo: 'https://github.com/hitchhooker/seer.git', // Update to point to your repository
         user: {
-            name: 'Samuele de Tomasi', // update to use your name
-            email: 'samuele@stranianelli.com' // Update to use your email
-        },
+			name: 'Tommi Thaumaturge', // update to use your name
+			email: 'tommi@niemi.lol' // Update to use your email
+		},
         dotfiles: true
     },
     () => {
